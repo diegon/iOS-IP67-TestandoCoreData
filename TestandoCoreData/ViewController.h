d945b1cdff1ea0c98f1ca914e06eda6026bc5950
Diego Negrelli <diegon@gmail.com>
2015-10-03 15:24:15 -0300

    Initial Commit

---
+++ TestandoCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestandoCoreData
+//
+//  Created by ios5380 on 03/10/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestandoCoreDataTests/TestandoCoreDataTests.m
@@ -0,0 +1,40 @@
+//
+//  TestandoCoreDataTests.m
+//  TestandoCoreDataTests
+//
+//  Created by ios5380 on 03/10/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TestandoCoreDataTests : XCTestCase
+
+@end
+
+@implementation TestandoCoreDataTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
